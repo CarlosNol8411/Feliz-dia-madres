afaf17ad8ee14630ac9a31ca2de2ae94fcce442c
CarlosNol8411 <cn8411@gmail.com>
2025-05-10 16:44:18 -0600

    terminado

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mostrarMensaje() {
+  Swal.fire({
+    title: '🌷 ¡Feliz Día de las Madres!',
+    text: 'Gracias por tu amor, tu fuerza y tu ternura. ¡Hoy es tu día!',
+    icon: 'success',
+    confirmButtonText: '💖 Un abrazo',
+    confirmButtonColor: '#b3426c'
+  });
+}
